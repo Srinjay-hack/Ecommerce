94fba224c57c3db0c45cdd806123ccddfd07b73e
Srinjay-hack <srinjaymondal@gmail.com>
2022-02-27 21:46:13 +0530

    new

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -4,7 +4,6 @@ from django.urls import path,include,re_path
 from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf.urls.static import static
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
