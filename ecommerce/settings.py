30a4a49f3fc9613adb0d0a760d7f8db619d4508c
Srinjay-hack <srinjaymondal@gmail.com>
2022-02-27 21:14:43 +0530

    deploy

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7v*njp6amgrw33==l71q*x@9urnw&c83*ft3s&+%h&4=l&&8^z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
